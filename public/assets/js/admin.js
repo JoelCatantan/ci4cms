920bfbd24338fc3f85e26451500f46891078003d
Joel BC <jbcatantan@gmail.com>
2019-08-26 13:47:04 +0800

    jo_menu tweak, right-nav

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -29,5 +29,23 @@ $(function() {
     function fixedHeight() {
         let window_height = $(window).height()
         $('main').css({'min-height': window_height + 'px'})
+        $('.right-nav-content').css({height: (window_height - 100) + 'px'})
     }
+
+    $('.right-nav-part-icon a').click(function() {
+        let is_selected = $(this).hasClass('active')
+
+        $('.right-nav-part-icon a.active').removeClass('active')
+        $('.right-nav-part-icon.active').removeClass('active')
+        $('.right-nav-container.active').removeClass('active')
+        $('.right-nav-content > div.active').removeClass('active')
+
+        if (!is_selected) {
+            let selected_container = $(this).data('targetContainer')
+            $(`.${selected_container}-container`).addClass('active')
+            $(this).addClass('active')
+            $('.right-nav-part-icon').addClass('active')
+            $('.right-nav-container').addClass('active')
+        }
+    })
 })
